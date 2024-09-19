97a65a3de3c9aea75f8faa8ea214acde42d0cd2a
amalendu123 <220229@tkmce.ac.in>
2024-09-19 17:12:31 +0530

    ..

--- functions/views.py
+++ functions/views.py
@@ -52,6 +52,7 @@ def run_selenium_script(username, password):
     options.add_argument('--no-sandbox')
     options.add_argument('--window-size=1920,1080')
     options.add_argument('--headless')
+    
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
 
